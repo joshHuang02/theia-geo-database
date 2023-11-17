091f102e3d161d0efee0f9edda1f93c71b8556cf
joshHuang02 <52260865+joshHuang02@users.noreply.github.com>
2023-11-17 15:25:55 -0500

    completed basic CRUD operations

--- theia-geo-database-backend/routes/routes.js
+++ theia-geo-database-backend/routes/routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 // const Model = require('../models/model');
 const FeatureCollection = require('../models/featureCollection');
 const bodyParser = require('body-parser');
+const featureCollection = require('../models/featureCollection');
 
 const router = express.Router()
 
@@ -23,6 +24,17 @@ router.post('/post/geoJSON', bodyParser.json(), async (req, res) => {
     }
     catch (error) {
         res.status(400).json({message: error.message})
+    }
+})
+
+//Get by ID Method
+router.get('/getOne/:id', async (req, res) => {
+    try{
+        const data = await featureCollection.findById(req.params.id);
+        res.json(data)
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
     }
 })
 
@@ -52,6 +64,24 @@ router.get('/featureCollections', async (req, res) => {
     }
 });
 
+//Update by ID Method
+router.patch('/update/:id', bodyParser.json(), async (req, res) => {
+    try {
+        const id = req.params.id;
+        const updatedData = req.body;
+        const options = { new: true };
+
+        const result = await featureCollection.findByIdAndUpdate(
+            id, updatedData, options
+        )
+
+        res.send(result)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+})
+
 //Delete by ID Method
 router.delete('/delete/:id', async (req, res) => {
     try {
@@ -74,44 +104,3 @@ router.delete('/deleteAll', async (req, res) => {
         res.status(400).json({ message: error.message })
     }
 })
-
-// //Get by ID Method
-// router.get('/getOne/:id', async (req, res) => {
-//     try{
-//         const data = await Model.findById(req.params.id);
-//         res.json(data)
-//     }
-//     catch(error){
-//         res.status(500).json({message: error.message})
-//     }
-// })
-
-// //Update by ID Method
-// router.patch('/update/:id', bodyParser.json(), async (req, res) => {
-//     try {
-//         const id = req.params.id;
-//         const updatedData = req.body;
-//         const options = { new: true };
-
-//         const result = await Model.findByIdAndUpdate(
-//             id, updatedData, options
-//         )
-
-//         res.send(result)
-//     }
-//     catch (error) {
-//         res.status(400).json({ message: error.message })
-//     }
-// })
-
-// //Delete by ID Method
-// router.delete('/delete/:id', async (req, res) => {
-//     try {
-//         const id = req.params.id;
-//         const data = await Model.findByIdAndDelete(id)
-//         res.send(`Document with ${data.name} has been deleted..`)
-//     }
-//     catch (error) {
-//         res.status(400).json({ message: error.message })
-//     }
-// })
