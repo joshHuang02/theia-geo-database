7b2c95d07b80e4c481f0401810a67f45f1ae12be
joshHuang02 <52260865+joshHuang02@users.noreply.github.com>
2023-12-04 19:12:42 -0500

    added function documentation to router

--- theia-geo-database-backend/routes/routes.js
+++ theia-geo-database-backend/routes/routes.js
@@ -11,14 +11,26 @@ const router = express.Router()
 
 module.exports = router;
 
-// Post geoJSON Method
+/* 
+    This route is for uploading a geoJSON feature collection.
+    The feature collection is validated using the geojson-validation library.
+    The feature collection without feature details is then saved to the database and returned in the response.
+
+    Example query: http://localhost:3000/post/geoJSON
+*/
 router.post('/post/geoJSON', bodyParser.json(), async (req, res) => {
     const data = await geoJson.postCollection(req.body);
     res.status(data[0]).json(data[1]);
 });
 
-// Post KML Method
-// parsed as text, xml is validated during conversion to geoJSON
+/* 
+    This route is for uploading a KML feature collection.
+    KML is parsed as text in request body.
+    The KML is then converted to geoJSON using the togeojson library, KML is validated at this step.
+    The geoJSON is then saved to the database and returned in the response.
+
+    Example query: http://localhost:3000/post/kml
+*/
 router.post('/post/kml', bodyParser.text(), async (req, res) => {
     const kmlToGeoJson = await kmlConverter.convertToGeoJSON(req.body);
     if (kmlToGeoJson[0] != 200) res.status(kmlToGeoJson[0]).json(kmlToGeoJson[1]);
@@ -27,13 +39,24 @@ router.post('/post/kml', bodyParser.text(), async (req, res) => {
     res.status(data[0]).json(data[1]);
 });
 
-//Get geoJSON collection by ID Method
+/*
+    This route is for getting a geoJSON feature collection by ID.
+    The feature collection is retrieved from the database as geoJSON and returned in the response.
+
+    Example query: http://localhost:3000/getOne/featureCollection/5f9b3b7b9c9b8c2b3c8b3b3b
+*/
 router.get('/getOne/featureCollection/:id', async (req, res) => {
     const data = await geoJson.getCollectionById(req);
     res.status(data[0]).json(data[1]);
 });
 
-// Get kml collection by ID Method
+/*
+    This route is for getting a KML feature collection by ID.
+    The feature collection is retrieved from the database as geoJSON and converted to KML using the tokml library.
+    The KML is then returned in the response.
+
+    Example query: http://localhost:3000/getOne/kmlCollection/5f9b3b7b9c9b8c2b3c8b3b3b
+*/
 router.get('/getOne/kmlCollection/:id', async (req, res) => {
     const data = await geoJson.getCollectionById(req);
     if (data[0] != 200) res.status(data[0]).json(data[1]);
@@ -42,25 +65,49 @@ router.get('/getOne/kmlCollection/:id', async (req, res) => {
     res.status(geoJsonToKml[0]).send(geoJsonToKml[1]);
 });
 
-// Get geoJSON feature by ID Method, does not have a kml equivalent
+/*
+    This route is for getting a geoJSON feature by ID and does not have a KML equivalent since individual features cannot be converted to KML.
+    The feature is retrieved from the database as geoJSON and returned in the response.
+
+    Example query: http://localhost:3000/getOne/feature/5f9b3b7b9c9b8c2b3c8b3b3b
+*/
 router.get('/getOne/feature/:id', async (req, res) => {
     const data = await geoJson.getFeatureById(req);
     res.status(data[0]).json(data[1]);
 });
 
-// Get all feature collections, does not have a kml equivalent
+/*
+    This route is for getting all feature collections as geoJSON and does not have a kml equivalent since array of collections cannot be converted to KML.
+    The route can be queried to include features in the response by setting parameter "includeFeatures" to "true".
+    The feature collections array is retrieved from the database as geoJSON and returned in the response.
+    This route should be used for debugging purposes only.
+
+    Example query: http://localhost:3000/featureCollections?includeFeatures=true
+*/
 router.get('/featureCollections', async (req, res) => {
     const data = await geoJson.getAllCollections(req);
     res.status(data[0]).json(data[1]);
 });
 
-// Get all features
+/*
+    This route is for getting all features as geoJSON.
+    The features are retrieved from the database as geoJSON and returned in the response.
+
+    Example query: http://localhost:3000/features
+*/
 router.get('/features', async (req, res) => {
     const data = await geoJson.getAllFeatures();
     res.status(data[0]).json(data[1]);
 });
 
 // Get all features in KML file
+/*
+    This route is for getting all features as KML.
+    The features are retrieved from the database as geoJSON and converted to KML using the tokml library.
+    The KML is then returned in the response.
+
+    Example query: http://localhost:3000/features/kml
+*/
 router.get('/features/kml', async (req, res) => {
     const data = await geoJson.getAllFeatures();
     if (data[0] != 200) res.status(data[0]).json(data[1]);
@@ -70,12 +117,28 @@ router.get('/features/kml', async (req, res) => {
 });
 
 // Get geoJSON features within polygon
+/*
+    This route is for getting all features within a polygon as geoJSON.
+    The features within the polygon are retrieved from the database as geoJSON and returned in the response.
+
+    Example query: http://localhost:3000/getFeaturesWithinPolygon
+    Example body: {
+    "coordinates": [[
+        [ -116.9156922714257,33.00283000223396],
+        [-116.8902399997034,33.00172878565257],
+        [-116.8908543579805,33.01513716478932],
+        [-116.9148893692876,33.0162159254125],
+        [-116.9156922714257,33.00283000223396]]]
+    }
+*/
 router.get('/getFeaturesWithinPolygon', bodyParser.json(), async (req, res) => {
     const data = await geoJson.getFeaturesWitinPolygon(req);
     res.status(data[0]).json(data[1]);
 });
 
-// Get KML features within polygon
+/* 
+    This route does the same thing as "getFeaturesWithinPolygon", but converts the results to KML using the tokml library.
+*/
 router.get('/getFeaturesWithinPolygon/kml', bodyParser.json(), async (req, res) => {
     const data = await geoJson.getFeaturesWitinPolygon(req);
     if (data[0] != 200) res.status(data[0]).json(data[1]);
@@ -84,13 +147,24 @@ router.get('/getFeaturesWithinPolygon/kml', bodyParser.json(), async (req, res) 
     res.status(geoJsonToKml[0]).send(geoJsonToKml[1]);
 });
 
-// Get geoJson features withing circle
+/*
+    This route is for getting all features within a circle as geoJSON.
+    The features within the circle are retrieved from the database as geoJSON and returned in the response.
+
+    Example query: http://localhost:3000/getFeaturesWithinCircle
+    Example body: {
+    "center": [-116.90771795879387, 33.012529527466185],
+    "radius": 0.5
+    }
+*/
 router.get('/getFeaturesWithinCircle', bodyParser.json(), async (req, res) => {
     const data = await geoJson.getFeaturesWithinCircle(req);
     res.status(data[0]).json(data[1]);
 });
 
-// Get KML features within circle
+/*
+    This route does the same thing as "getFeaturesWithinCircle", but converts the results to KML using the tokml library.
+*/
 router.get('/getFeaturesWithinCircle/kml', bodyParser.json(), async (req, res) => {
     const data = await geoJson.getFeaturesWithinCircle(req);
     if (data[0] != 200) res.status(data[0]).json(data[1]);
@@ -99,10 +173,12 @@ router.get('/getFeaturesWithinCircle/kml', bodyParser.json(), async (req, res) =
     res.status(geoJsonToKml[0]).send(geoJsonToKml[1]);
 });
 
-//Update collection by ID Method
-// Will not implement this route due to the way features and featureCollections are stored separately in the database, as well as the fact that collections are only uploaded as a whole document and usually does not require updating individual features.
-// Instead, delete the old collection and upload a new one.
-router.patch('/update/:id', bodyParser.json(), async (req, res) => {
+/*
+    Update collection by ID Method
+    Will not implement this route due to the way features and featureCollections are stored separately in the database, as well as the fact that collections are only uploaded as a whole document and usually does not require updating collection details.
+    Instead, delete the old collection and upload a new one.
+*/
+router.patch('/updateCollection/:id', bodyParser.json(), async (req, res) => {
     res.status(400).json({ message: "Delete the old collection and upload a new one" })
     // try {
     //     const id = req.params.id;
@@ -120,8 +196,20 @@ router.patch('/update/:id', bodyParser.json(), async (req, res) => {
     // }
 })
 
-/* Update feature by ID Method
-   This route is only for debugging, as individual features are not meant to be updated.
+/*
+    This route is for updating a feature by ID.
+    The feature is retrieved from the database and updated with the request body.
+    The updated feature is then saved to the database and returned in the response.
+    This route is only for debugging, as individual features are not meant to be updated.
+
+    Example query: http://localhost:3000/updateFeature/5f9b3b7b9c9b8c2b3c8b3b3b
+    Example body: {
+    "type": "Feature",
+    "properties": {
+        "name": "New Feature"
+    },
+    "geometry": {...}
+    }
 */
 router.patch('/updateFeature/:id', bodyParser.json(), async (req, res) => {
     try {
@@ -144,7 +232,14 @@ router.patch('/updateFeature/:id', bodyParser.json(), async (req, res) => {
     }
 });
 
-//Delete feature collection by ID Method
+/*
+    This route is for deleting a feature collection by ID.
+    The feature collection is retrieved from the database and deleted.
+    The features in the collection are also deleted from the database.
+    The deleted feature collection is returned in the response.
+
+    Example query: http://localhost:3000/deleteFeatureCollection/5f9b3b7b9c9b8c2b3c8b3b3b
+*/
 router.delete('/deleteFeatureCollection/:id', async (req, res) => {
     try {
         const id = req.params.id;
@@ -165,7 +260,14 @@ router.delete('/deleteFeatureCollection/:id', async (req, res) => {
     }
 })
 
-// Delete feature by ID Method
+/*
+    This route is for deleting a feature by ID.
+    The feature is retrieved from the database and deleted.
+    The deleted feature is returned in the response.
+    This route is only for debugging, as individual features are not meant to be deleted.
+
+    Example query: http://localhost:3000/deleteFeature/5f9b3b7b9c9b8c2b3c8b3b3b
+*/
 router.delete('/deleteFeature/:id', async (req, res) => {
     try {
         const id = req.params.id;
@@ -186,7 +288,13 @@ router.delete('/deleteFeature/:id', async (req, res) => {
     }
 })
 
-//Delete all method
+/*
+    This route is for deleting all feature collections and features.
+    All feature collections and features are deleted from the database.
+    This route is mostly for debugging purposes.
+
+    Example query: http://localhost:3000/deleteAll
+*/
 router.delete('/deleteAll', async (req, res) => {
     try {
         const deleteCollections = await FeatureCollection.deleteMany({})
